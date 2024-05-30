4d8f277ec7efacb0eb00e8e6231addf09c0257dd
yernar <yernar.baiginzheyev@gmail.com>
2024-05-31 01:59:49 +0500

    Initial Commit

---
+++ coordinatorPattern/MainModule/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  coordinatorPattern
+//
+//  Created by Yernar Baiginzheyev on 31.05.2024.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
---
+++ coordinatorPattern/SecondModule/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  coordinatorPattern
+//
+//  Created by Yernar Baiginzheyev on 31.05.2024.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
